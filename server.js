d0b356fc01158517453cd6b2a8f11f6a7fe7818d
Saimon Alam <ibnshafique@gmail.com>
2016-09-11 21:01:11 +0600

    commit after rollback

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+ socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log('message: '+msg);
+  });
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+//setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
