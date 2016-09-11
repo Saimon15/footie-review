05de9dd12c68f5451eb76a7ab3fd2a8a399006d8
Saimon Alam <ibnshafique@gmail.com>
2016-09-11 22:06:28 +0600

    another commit.another try

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 const express = require('express');
 const socketIO = require('socket.io');
@@ -23,4 +23,37 @@ io.on('connection', (socket) => {
   });
 });
 
-//setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+*/
+
+var express = require('express');
+var app = express();
+app.set('port', (process.env.PORT || 3000));
+
+var server = app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+var io = require('socket.io')(server);
+
+app.use(express.static("./views"));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.get('/', function (req, res) {
+    var path = __dirname + '/index.html';
+    console.log(path);
+    res.sendFile(path);
+});
+
+io.on('connection', function(socket) {
+	socket.emit('chat message', 'get down wid it');
+    socket.on('beep', function(){
+        socket.emit("beep", {data: 5});
+        console.log('beep recieved');
+    });
+});
